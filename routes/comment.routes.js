bbc75b425b654c2d8d4dd9fda5dacd37ff0665d2
Ana Filipa <anafilipa737@gmail.com>
2024-03-09 13:24:50 +0000

    comment.routes updates - replying and liking option

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
-
-const express = require('express');
 const DanceClass = require('../models/DanceClass.model');
 const { isAuthenticated } = require("../middleware/jwt.middleware");
 
 const app = express();
 
- // POST /dance-classes/:id/comments - Add a comment to a specific dance class
- router.post("/dance-classes/:id/comments", isAuthenticated, (req, res) => {
+// POST /dance-class/:class/comment
+// Create a new comment on a specific class by ID
+// Both teachers and students can leave comments and replies
+// (Edit and delete comments as a bonus would be very nice)
+
+ // POST /dance-classes/:id/comment - Add a comment to a specific dance class
+ router.post("/dance-classes/:id/comment", isAuthenticated, (req, res) => {
     const danceClassId = req.params.id;
     const { comment } = req.body;
     const userId = req.payload._id;
@@ -20,11 +23,24 @@ const app = express();
           return res.status(404).json({ error: "Dance class not found." });
         }
   
-        // new comment
+        // new comment with the LikesCount
         const newComment = {
           text: comment,
-          user: userId
+          user: userId,
+          likesCount: 0, // Initialize likesCount to 0
         };
+
+        // If it's a reply, update the parent comment with the new reply
+        if (parentCommentId) {
+        const parentComment = danceClass.comments.id(parentCommentId);
+        if (parentComment) {
+          parentComment.replies.push(newComment);
+        }
+      } else {
+        
+        // Add the comment to the main comments array
+        danceClass.comments.push(newComment);
+      }
   
         // Add the comment to the array
         danceClass.comments.push(newComment);
